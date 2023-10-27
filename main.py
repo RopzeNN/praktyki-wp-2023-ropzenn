b79d752131f049720391b25565b2c3b08fe4ab68
RopzeNN <104526351+RopzeNN@users.noreply.github.com>
2023-10-27 12:02:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,48 +2,44 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
-
-def informacje_o_produkcie(query):
+def get_product_info(query, attempt=0):
+    if attempt >= 3:
+        print("Błąd , podaj inną nazwę")
+        return "Brak nazwy", "Brak oceny", "Brak ceny"
     ans = query.replace(" ", "+")
     url = f'https://www.amazon.pl/s?k={ans}'
     try:
         response = requests.get(url, timeout=10)
         soup = BeautifulSoup(response.content, 'html.parser')
-        przedmiot = soup.select('div.a-section.a-spacing-base')[0]
-        rating_element = przedmiot.select('span.a-icon-alt')
-      
+        item = soup.select('div.a-section.a-spacing-base')[0]
+        rating_element = item.select('span.a-icon-alt')
         if rating_element:
             rating = rating_element[0].text
         else:
             rating = "Brak oceny"
-            nazwa_produktu = przedmiot.select('h2.a-size-mini.a-spacing-none.a-color-base.s-line-clamp-4')
-          
-        if nazwa_produktu:
-            nazwa = nazwa_produktu[0].text
+        name_element = item.select('h2.a-size-mini.a-spacing-none.a-color-base.s-line-clamp-4')
+        if name_element:
+            name = name_element[0].text
         else:
-            nazwa = "Brak nazwy"
-        price_element = przedmiot.select('span.a-price span.a-offscreen')
-      
+            name = "Brak nazwy"
+        price_element = item.select('span.a-price span.a-offscreen')
         if price_element:
             price = price_element[0].get_text()
         else:
             price = "Brak ceny"
-        return nazwa, rating, price
-      
+        return name, rating, price
     except:
-        print("Nie udało się znaleźć produktu")
+        print("Błąd. Ponowna próba wyszukania produktu")
         time.sleep(5)
-        return informacje_o_produkcie(query)
-
+        return get_product_info(query, attempt + 1)
 
 def main():
-    query = input("Podaj nazwę produktu: ")
-    nazwa, rating, ocena = informacje_o_produkcie(query)
+    query = input("Jaki produkt chcesz wyszukać? :")
+    name, rating, price = get_product_info(query)
     print('Nazwa produktu:', name)
     print('Cena produktu:', price)
     print('Ocena produktu:', rating)
 
-    input("Press Enter to continue...")
-
+    input("Wciśnij enter aby kontynuować...")
 
 main()
